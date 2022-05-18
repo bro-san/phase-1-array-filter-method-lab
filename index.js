d8a94d2c92bfdbbbb0a950399d9535a78b9e4a66
brody.sanford@gmail.com <brodysmac@Brodens-MBP.fios-router.home>
2022-05-18 14:38:43 -0400

    commit tree 6abb4f9719b109bb0dc680fd84e2df1a9ebaf648

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(array, str) {
+    return array.filter(function(name) {return name.toLowerCase() === str.toLowerCase()})
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter(function(name) {return name.slice(0,1) === str.slice(0,1)})
+}
+
+function matchName(array, str) {
+    return array.filter(element => element.name === str)
+}
